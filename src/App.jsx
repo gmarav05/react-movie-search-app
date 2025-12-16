075e3871541a4ef7f38a6dd3d710f1e5dd3f952d
Aravind <gmarav005@gmail.com>
2025-12-16 20:33:48 +0530

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import SearchMovies from "./SearchMovie"
+
 function App() {
 
   return (
     <>
-      <div className="container">
+     <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies/>
       </div>
       
     </>
