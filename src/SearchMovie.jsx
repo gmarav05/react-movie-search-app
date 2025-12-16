76e5563132355e7a4f505984670be318cbdffe67
Aravind <gmarav005@gmail.com>
2025-12-16 22:51:57 +0530

    Add new Movie component

--- src/SearchMovie.jsx
+++ src/SearchMovie.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { useState } from "react";
+import Movie from "./Movie";
 
 export default function SearchMovies(){
 
+    const [query, setQuery] = useState('')
+    const [movies, setMovies] = useState([])
+
     const searchMovie = async (e) => {
         e.preventDefault();
-
-        const query = "Bahuballi";
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=7092aa45ca0fadbd5c68a939f0543272&language=en-US&query=${query}&page=1&include_adult=false`
 
@@ -13,6 +15,7 @@ export default function SearchMovies(){
             const res = await fetch(url);
             const data = await res.json()
             console.log(data)
+            setMovies(data.results)
         } catch(err) {
             console.error(err);
         }
@@ -20,11 +23,23 @@ export default function SearchMovies(){
 
 
     return (
+        <>
         <form className="form" onSubmit={searchMovie}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"/>
+                placeholder="i.e. Jurassic Park" value={query} onChange={(e) => setQuery(e.target.value)} />
             <button className="button" type="submit">Search</button>
         </form>
+        <div className="card-list">
+            {movies.filter(movie => movie.poster_path).map( movie => (
+                
+                  <Movie movie={movie} key={movie.id} />
+
+            ))}
+
+        </div>
+
+
+        </>
     )
 }
